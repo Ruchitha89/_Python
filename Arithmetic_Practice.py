294b13e74e2dd703deac7b5e9663ea251d12de0f
Ruchitha89 <ruchithahjjagadeesh@gmail.com>
2026-09-02 22:27:56 +0530

    Initial commit

---
+++ Arithmetic_Practice.py
@@ -0,0 +1,22 @@
+""" Arithmetic Practice : Write a Python Program that performs basic arithmetic operations
+(addition, subtraction, multiplication, and division) on two numbers.Define the two numbers 
+as variables within the code and print the results of each operation. """
+
+# Store two numbers in variables
+
+num1 = 10
+num2 = 5
+
+# Perform arithmetic operations
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Print the results
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
