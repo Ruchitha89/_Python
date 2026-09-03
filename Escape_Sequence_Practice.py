789d8a3f64fd036e554c828d8de3de734aeeb2cc
Ruchitha89 <ruchithahjjagadeesh@gmail.com>
2026-09-03 21:49:57 +0530

    Add escape sequence practice script

---
+++ Escape_Sequence_Practice.py
@@ -0,0 +1,15 @@
+"""Escape Sequence Practice : Write a Python program that uses escape sequences to print the following output:
+    Example :
+    Hello
+         world
+    This is a backslash: \\ 
+"""
+
+a = "Hello\n\tworld\nThis is a backslash: \\"
+print(a)
+
+# OR
+
+print("Hello")
+print("\tworld")
+print("This is a backslash: \\")
